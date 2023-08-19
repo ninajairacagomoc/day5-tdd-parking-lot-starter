ce81961729d1aa088fd3e9859aec261e894fbba3
cagomni <cagomni@oocl.com>
2023-08-19 23:14:46 +0800

    feat: modified should_return_ticket_when_park_given_parkingLot_and_a_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,4 +1,12 @@
 package com.parkinglot;
 
 public class ParkingLot {
+    public ParkingTicket park(Car car) {
+        return new ParkingTicket();
+    }
+    public Car fetch(ParkingTicket parkingTicket) {
+        return null;
+    }
+
+
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,23 +2,31 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
+
+    @Test
+    void should_return_ticket_when_park_given_parkingLot_and_a_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        //when
+        ParkingTicket parkingTicket = parkingLot.park(car);
+        //then
+        assertNotNull(parkingTicket);
+    }
+
 //    @Test
-//    void should(){
+//    void should_return_the_car_when_fetch_given_parkingLot_and_a_ticket() {
 //        //given
+//        ParkingLot parkingLot = new ParkingLot();
+//        Car car = new Car();
+//        ParkingTicket parkingTicket = new ParkingTicket();
 //        //when
+//        Car fetchCar = parkingLot.fetch(parkingTicket);
 //        //then
+//        assertEquals(car,fetchCar);
 //    }
-@Test
-void should_return_ticket_when_park_given_parkingLot_and_a_car(){
-    //given
-    ParkingLot parkingLot = new ParkingLot();
-    Car car = new Car();
-    //when
-    ParkingTicket parkingTicket = new ParkingTicket();
-    //then
-    assertNotNull(parkingTicket);
-}
 }
