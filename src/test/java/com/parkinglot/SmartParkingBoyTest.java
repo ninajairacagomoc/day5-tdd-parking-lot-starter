fe0c3e3c4147753a4a331d3629db6d1f76c93b4a
ninajairacagomoc <jairalael101@gmail.com>
2023-08-20 15:16:37 +0800

    feat: should_return_nothing_with_No_available_position_when_park_given_a_smart_parking_boy_and_two_parking_lot_and_both_without_position_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -1,5 +1,6 @@
 package com.parkinglot;
 
+import com.parkinglot.exception.NoAvailablePositionException;
 import com.parkinglot.exception.UnrecognizedTicketException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -98,6 +99,21 @@ public class SmartParkingBoyTest {
         );
         //then
         assertEquals("Unrecognized parkingTicket", unrecognizedTicketException.getMessage());
+    }
+    @Test
+    void should_return_nothing_with_No_available_position_when_park_given_a_smart_parking_boy_and_two_parking_lot_and_both_without_position_and_a_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(0);
+        ParkingLot secondParkingLot = new ParkingLot(0);
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        StandardParkingBoy smartParkingBoy = new StandardParkingBoy(parkingLots);
+        Car car = new Car();
+        //when
+        NoAvailablePositionException noAvailablePositionException = assertThrows(NoAvailablePositionException.class, () ->
+                smartParkingBoy.park(car, firstParkingLot)
+        );
+        //then
+        assertEquals("No available parkingLot position", noAvailablePositionException.getMessage());
     }
 
 }
