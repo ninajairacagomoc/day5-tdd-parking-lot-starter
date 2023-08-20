6b3580bbacd3bc90e23764c042113c0475f85daf
ninajairacagomoc <jairalael101@gmail.com>
2023-08-20 10:19:19 +0800

    feat: should_return_the_car_when_fetch_given_parkingLot_a_standard_parking_boy_and_a_car

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -4,4 +4,8 @@ public class StandardParkingBoy {
     public ParkingTicket park(Car car,ParkingLot parkingLot) {
         return parkingLot.park(car);
     }
+
+    public Car fetch(ParkingTicket parkingTicket,ParkingLot parkingLot) {
+        return parkingLot.fetch(parkingTicket);
+    }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -20,74 +20,18 @@ public class ParkingLotTest {
         assertNotNull(parkingTicket);
     }
 
-//    @Test
-//    void should_return_the_car_when_fetch_given_parkingLot_and_a_ticket() {
-//        //given
-//        ParkingLot parkingLot = new ParkingLot();
-//        Car car = new Car();
-//        ParkingTicket parkingTicket = parkingLot.park(car);
-//        //when
-//        Car fetchCar = parkingLot.fetch(parkingTicket);
-//        //then
-//        assertEquals(car, fetchCar);
-//    }
-//
-//    @Test
-//    void should_return_the_right_car_with_each_ticket_when_fetch_car_twice_given_a_parkingLot_and_two_parked_tickets() {
-//        //given
-//        ParkingLot parkingLot = new ParkingLot();
-//        Car firstCar = new Car();
-//        Car secondCar = new Car();
-//        ParkingTicket parkingTicketFirst = parkingLot.park(firstCar);
-//        ParkingTicket parkingTicketSecond = parkingLot.park(secondCar);
-//        //when
-//        Car fetchCarFirst = parkingLot.fetch(parkingTicketFirst);
-//        Car fetchCarSecond = parkingLot.fetch(parkingTicketSecond);
-//        //then
-//        assertEquals(firstCar, fetchCarFirst);
-//        assertEquals(secondCar, fetchCarSecond);
-//    }
-//
-//    @Test
-//    void should_return_unrecognizedTicketException_when_fetch_the_car_given_a_parkingLot_and_a_wrong_parking_ticket() {
-//        //given
-//        ParkingLot parkingLot = new ParkingLot();
-//        Car car = new Car();
-//        parkingLot.park(car);
-//        ParkingTicket wrongParkingTicket = new ParkingTicket();
-//        //when
-//        UnrecognizedTicketException unrecognizedTicketException = assertThrows(UnrecognizedTicketException.class, () ->
-//                parkingLot.fetch(wrongParkingTicket));
-//        //then
-//        assertEquals("Unrecognized parkingTicket", unrecognizedTicketException.getMessage());
-//    }
-//
-//    @Test
-//    void should_return_UnrecognizedTicketException_when_fetch_the_car_given_a_parkingLot_and_a_used_parking_ticket() {
-//        //given
-//        ParkingLot parkingLot = new ParkingLot();
-//        Car car = new Car();
-//        ParkingTicket usedTicket = parkingLot.park(car);
-//        parkingLot.fetch(usedTicket);
-//        //when
-//        UnrecognizedTicketException unrecognizedTicketException = assertThrows(UnrecognizedTicketException.class, () ->
-//                parkingLot.fetch(usedTicket));
-//        //then
-//        assertEquals("Unrecognized parkingTicket", unrecognizedTicketException.getMessage());
-//    }
-//
-//    @Test
-//    void should_return_UnrecognizedTicketException_when_park_the_car_given_a_parkingLot_without_any_position() {
-//        //given
-//        ParkingLot parkingLot = new ParkingLot(1);
-//        Car newCar = new Car();
-//        Car parkedCar = new Car();
-//        parkingLot.park(parkedCar);
-//        //when
-//        NoAvailablePositionException noAvailablePositionException = assertThrows(NoAvailablePositionException.class, () -> {
-//            parkingLot.park(newCar);
-//        });
-//        //then
-//        assertEquals("No available parkingLot position", noAvailablePositionException.getMessage());
-//    }
+    @Test
+    void should_return_the_car_when_fetch_given_parkingLot_a_standard_parking_boy_and_a_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy();
+        Car car = new Car();
+        ParkingTicket parkingTicket = standardParkingBoy.park(car,parkingLot);
+        //when
+        Car fetchCar = standardParkingBoy.fetch(parkingTicket,parkingLot);
+        //then
+        assertEquals(car, fetchCar);
+    }
+
+
 }
