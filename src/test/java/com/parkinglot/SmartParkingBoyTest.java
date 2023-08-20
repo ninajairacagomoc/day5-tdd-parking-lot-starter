f454f6102b9e116812527c93c6edc8c22e611fd4
ninajairacagomoc <jairalael101@gmail.com>
2023-08-20 14:48:31 +0800

    feat: should_park_to_second_parking_lot_when_park_given_a_smart_parking_boy_and_first_is_full_and_second_is_not_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -22,5 +22,22 @@ public class SmartParkingBoyTest {
         Assertions.assertEquals(3, firstParkingLot.getAvailableCapacity());
         Assertions.assertEquals(4, secondParkingLot.getAvailableCapacity());
     }
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_a_smart_parking_boy_and_first_is_full_and_second_is_not_and_a_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(10);
+        firstParkingLot.park(new Car());
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+        //then
+        Assertions.assertNotNull(parkingTicket);
+        Assertions.assertEquals(0, firstParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(9, secondParkingLot.getAvailableCapacity());
+    }
+
 }
 
--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -29,7 +29,7 @@ public class StandardParkingBoyTest {
     }
 
     @Test
-    void should_park_to_second_parking_lot_when_park_given_a_standard_parking_boy_and_first_is_full_and_second_isnot_and_a_car() {
+    void should_park_to_second_parking_lot_when_park_given_a_standard_parking_boy_and_first_is_full_and_second_is_not_and_a_car() {
         //given
         ParkingLot firstParkingLot = new ParkingLot(1);
         ParkingLot secondParkingLot = new ParkingLot();
