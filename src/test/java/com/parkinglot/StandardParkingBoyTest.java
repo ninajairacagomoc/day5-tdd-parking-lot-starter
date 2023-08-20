a6939463372ed2000cda44a9c573e1858703f015
ninajairacagomoc <jairalael101@gmail.com>
2023-08-20 12:06:14 +0800

    feat: should_park_to_second_parking_lot_when_park_given_a_standard_parking_boy_and_first_is_full_and_second_isnot_and_a_car

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class StandardParkingBoyTest {
     @Test
-    void should_park_to_first_parking_lot_when_park_given_a_standard_parking_boy_and_two_parking_lots_and_a_car(){
+    void should_park_to_first_parking_lot_when_park_given_a_standard_parking_boy_and_two_parking_lots_and_a_car() {
         //given
         ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
@@ -18,7 +18,25 @@ public class StandardParkingBoyTest {
         ParkingTicket parkingTicket = standardParkingBoy.park(car);
         //then
         Assertions.assertNotNull(parkingTicket);
-        Assertions.assertEquals(9,firstParkingLot.getAvailableCapacity());
-        Assertions.assertEquals(10,secondParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(9, firstParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_a_standard_parking_boy_and_first_is_full_and_second_isnot_and_a_car() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot();
+        firstParkingLot.park(new Car());
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
+        Car car = new Car();
+        //when
+        ParkingTicket parkingTicket = standardParkingBoy.park(car);
+
+        //then
+        Assertions.assertNotNull(parkingTicket);
+        Assertions.assertEquals(0, firstParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(9, secondParkingLot.getAvailableCapacity());
     }
 }
